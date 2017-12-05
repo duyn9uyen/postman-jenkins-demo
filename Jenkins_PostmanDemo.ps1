87f6d18dfd44d7f75fe43dcbddca52a362ded005
Duy Nguyen <dnguyen@captechconsulting.com>
2017-12-04 22:31:52 -0500

    inital commit of all files

---
+++ Jenkins_PostmanDemo.ps1
@@ -0,0 +1,25 @@
+#Windows Batch Command
+#powershell.exe -executionpolicy bypass -command "& '%WORKSPACE%\Jenkins_PostmanDemo.ps1'"
+
+echo "You are in the powershell script now..."
+
+$SourceFilePath = $env:WORKSPACE
+$FilenamePostfix = "*.postman_collection.json"
+$EnvironmentFile = "*.postman_environment.json"
+
+# Get all Postman test files
+$JsonFiles = Get-ChildItem -Path $SourceFilePath -name -Filter $FilenamePostfix | Sort-Object -Property CreationTime -Descending
+
+# Change to directory where we have NodeJs installed.  Otherwise, the 'newman' command will not be recognized. 
+# You can install NPM and Newman as a user and copy the ~Roaming\npm directory in the C:\ drive.
+#cd C:\Users\[username]\AppData\Roaming\npm\node_modules\newman\bin
+cd C:\npm\node_modules\newman\bin
+
+# Loop through the json files and execute newman to run the Postman tests
+foreach ($File in $JsonFiles) {
+	$collectionfilepath = "$SourceFilePath\$File"
+	$environmentfilepath = "$SourceFilePath\$EnvironmentFile"
+	node newman run --disable-unicode $collectionfilepath -e $environmentfilepath
+}
+
+exit $LASTEXITCODE
---
+++ README.md
@@ -0,0 +1,1 @@
+# postman-jenkins-demo
